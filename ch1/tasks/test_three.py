dbb731fdf509ef89df90323309ebff7fe042571d
yusuke1225math2 <tambo@saiand.co.jp>
2024-04-23 05:23:41 +0900

    task_proj added

--- ch1/tasks/test_three.py
+++ ch1/tasks/test_three.py
@@ -1,5 +1,6 @@
 """Test the Task data type."""
 
+import pytest
 from collections import namedtuple
 
 Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
@@ -11,6 +12,7 @@ def test_defaults():
     t2 = Task(None, None, False, None)
     assert t1 == t2
 
+@pytest.mark.run_these_please
 def test_member_access():
     """Check .field functionality of namedtuple."""
     t = Task('buy milk', 'brian')
--- ch2/tasks_proj/tests/unit/test_task.py
+++ ch2/tasks_proj/tests/unit/test_task.py
@@ -1,8 +1,5 @@
-import pytest
-from collections import namedtuple
-
-Task = namedtuple('Task', ['summary', 'owner', 'done', 'id'])
-Task.__new__.__defaults__ = (None, None, False, None)
+"""Placeholder test file."""
+from tasks import Task
 
 def test_asdict():
     """_asdict() should return a dictionary."""
@@ -16,10 +13,22 @@ def test_asdict():
     }
     assert t_dict == expected
 
-@pytest.mark.run_these_please
 def test_replace():
     """_replace() should change passed in fields."""
     t_before = Task('finish book', 'brian', False)
     t_after = t_before._replace(id=10, done=True)
     t_expected = Task('finish book', 'brian', True, 10)
     assert t_after == t_expected
+
+def test_defaults():
+    """Using no parameters should invoke defaults."""
+    t1 = Task()
+    t2 = Task(None, None, False, None)
+    assert t1 == t2
+
+def test_member_access():
+    """Check .field functionality of namedtuple."""
+    t = Task('buy milk', 'brian')
+    assert t.summary == 'buy milk'
+    assert t.owner == 'brian'
+    assert (t.done, t.id) == (False, None)
